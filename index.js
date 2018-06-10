ca87258e5cc41d399e79377d67dbe644e93c6cbf
Nitehawk <nitehawk@nitespace.co>
2018-06-10 14:13:42 +0000

    Convert to async structure to facilitate addition of PostgeSQL

--- index.js
+++ index.js
@@ -3,34 +3,43 @@ const AWS = require('aws-sdk');
 const AWSMqtt = require('aws-mqtt');
 const WebSocket = require('ws');
 
-// Set a default AWS region - we'll make this more flexible later if needed
-AWS.config.region = 'us-east-1';
 
-// Initialize some global options
-var IOTOptions = {
-  WebSocket: WebSocket,
-  region: AWS.config.region,
-  credentials: AWS.config.credentials,
-  clientId: 'raspi-listen-' + (Math.floor((Math.random() * 100000) +1)),
-};
+// Wrap everything in an async function to enable use of await
+async function setup()
+{
+  // Set a default AWS region - we'll make this more flexible later if needed
+  AWS.config.region = 'us-east-1';
+
+  // Initialize some global options for IOT
+  var IOTOptions = {
+    WebSocket: WebSocket,
+    region: AWS.config.region,
+    credentials: AWS.config.credentials,
+    clientId: 'raspi-listen-' + (Math.floor((Math.random() * 100000) +1)),
+  };
+
+  try {
+    //  Get Endpoint
+    const IOT = new AWS.Iot()
+    const endpoint = await IOT.describeEndpoint().promise();
+    IOTOptions.endpoint = endpoint.endpointAddress;
+    //console.log(IOTOptions);
+    
+    // Setup IOT MQTT client
+    console.log("Building mqtt client");
+    const client = AWSMqtt.connect(IOTOptions);
+    client.on('connect', () => { client.subscribe('environment/data'); });
+    client.on('message', (topic, message) => {
+      data = JSON.parse(message);
+      data.tempf = Math.round((data.temp*1.8+32)*100)/100;
+      //vdc = data.vadcv * 
+      console.log(data.sensor, ": ", data.temp, "c / ", data.tempf, "f");
+    });
+    client.on('close', () => { console.log("Connection closed, exiting"); });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
 
 
-// Ask AWS for our IOT MQTT endpoint
-IOT = new AWS.Iot()
-IOT.describeEndpoint().promise().then(function(data) {
-  IOTOptions.endpoint = data.endpointAddress;
-})
-.catch(function(err) {console.log(err)})
-.then(function(data){
-  console.log("Building mqtt client");
-  const client = AWSMqtt.connect(IOTOptions);
-  client.on('connect', () => { client.subscribe('environment/data'); });
-  client.on('message', (topic, message) => {
-    data = JSON.parse(message);
-    data.tempf = Math.round((data.temp*1.8+32)*100)/100;
-    console.log(data.sensor, ": ", data.temp, "c / ", data.tempf, "f");
-  });
-  client.on('close', () => { console.log("Connection closed, exiting"); });
-})
-
-
+setup();
