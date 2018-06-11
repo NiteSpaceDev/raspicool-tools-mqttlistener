a6189f4bf097195d4ec748e68a634fb5ebf1d976
Nitehawk <nite97m@gmail.com>
2018-06-11 00:17:24 -0500

    Very basic dataloader setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const AWS = require('aws-sdk');
 const AWSMqtt = require('aws-mqtt');
 const WebSocket = require('ws');
+const { Client } = require('pg');
+
 
 
 // Wrap everything in an async function to enable use of await
@@ -18,24 +20,44 @@ async function setup()
     clientId: 'raspi-listen-' + (Math.floor((Math.random() * 100000) +1)),
   };
 
+  var pgconn = {
+	  user: 'sensor',
+	  host: 'timescaledb',
+	  database: 'raspicool',
+	  password: 'dataload',
+	  port: 5432 };
+
+  const query = 'INSERT into readings (sid, tempc, pressure, humidity, voltage) values ($1, $2, $3, $4, $5) returning *';
+
   try {
     //  Get Endpoint
     const IOT = new AWS.Iot()
     const endpoint = await IOT.describeEndpoint().promise();
     IOTOptions.endpoint = endpoint.endpointAddress;
     //console.log(IOTOptions);
+
+	  const pgclient = new Client(pgconn);
+    await pgclient.connect();
     
     // Setup IOT MQTT client
     console.log("Building mqtt client");
     const client = AWSMqtt.connect(IOTOptions);
-    client.on('connect', () => { client.subscribe('environment/data'); });
+    client.on('connect', async () => { 
+	    client.subscribe('environment/data'); 
+    });
     client.on('message', (topic, message) => {
       data = JSON.parse(message);
       data.tempf = Math.round((data.temp*1.8+32)*100)/100;
-      //vdc = data.vadcv * 
       console.log(data.sensor, ": ", data.temp, "c / ", data.tempf, "f");
+      if (topic == 'environment/data')
+	    {
+	      pgclient.query(query, [data.sensor, data.temp, data.pressure, data.humidity, data.voltage])
+	       .then( res => { console.log(res.rows[0]); } )
+	       .catch(e => console.error(e.stack));
+	    }
+
     });
-    client.on('close', () => { console.log("Connection closed, exiting"); });
+    client.on('close', () => { console.log("Connection closed, exiting");  });
   } catch (err) {
     console.log(err.message);
   }
